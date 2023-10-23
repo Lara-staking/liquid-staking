4c86dd59a34c4d42a501ca3be0bd5fed7495cfc2
VargaElod23 <vargaelod23@gmail.com>
2023-10-23 20:05:09 +0200

    remove outdated hardhat tests

--- test/util/ErrorsNames.ts
+++
@@ -1,7 +0,0 @@
-export enum ErrorsNames {
-    DepositAmountTooLow = 'DepositAmountTooLow',
-    InsufficientProtocolBalanceForBurn = 'InsufficientProtocolBalanceForBurn',
-    InsufficientUserBalanceForBurn = 'InsufficientUserBalanceForBurn',
-    StakeAmountTooLow = 'StakeAmountTooLow',
-    StakeValueTooLow = 'StakeValueTooLow',
-}
--- util/ContractsNames.ts
+++
@@ -1,7 +0,0 @@
-export enum ContractsNames {
-    stTara = 'stTARA',
-    apyOracle = 'ApyOracle',
-    lara = 'Lara',
-    nodeContinuityOracle = 'NodeContinuityOracle',
-    mockDpos = 'MockDpos',
-}
