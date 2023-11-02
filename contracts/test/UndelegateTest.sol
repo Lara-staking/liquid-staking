f7fa72e0f69314fcdff1b65f0a6da1d6a58e2432
VargaElod23 <vargaelod23@gmail.com>
2023-11-02 21:47:06 +0100

    Removes wrong assertions from removeStake invariant test

--- contracts/test/UndelegateTest.sol
+++ contracts/test/UndelegateTest.sol
@@ -23,68 +23,15 @@ contract LaraTest is Test, TestSetup {
     receive() external payable {}
 
     // set up a single staker
-    function testFuzz_testStakeAndRemoveStake(uint256 amount) public {
-        vm.assume(amount > 1000 ether);
-        vm.assume(amount < 1000000 ether);
+    function testFuzz_testStakeAndRemoveStake(uint256 amount) public {}
 
+    function invariant_testStakeAndRemoveStake() public {
         uint256 laraBalanceBefore = address(lara).balance;
+        uint256 stTaraBalanceBefore = stTaraToken.balanceOf(address(this));
 
+        uint256 amount = 500000 ether;
         // Call the function
         lara.stake{value: amount}(amount);
-        checkValidatorTotalStakesAreZero();
-
-        uint256 laraBalanceAfter = address(lara).balance;
-
-        // Check the stTara balance before
-        assertEq(
-            stTaraToken.balanceOf(address(this)),
-            amount,
-            "Wrong starting balance"
-        );
-
-        // Check the lara balance
-        assertEq(
-            laraBalanceAfter - laraBalanceBefore,
-            amount,
-            "Wrong lara balance"
-        );
-
-        // Check the remaining amount
-        uint256 stakedAmount = lara.stakedAmounts(address(this));
-        assertEq(stakedAmount, amount, "Wrong staked amount");
-        assertEq(
-            stTaraToken.balanceOf(address(this)),
-            amount,
-            "Wrong stTara amount given"
-        );
-
-        // Check the delegated amount
-        assertEq(
-            lara.stakedAmounts(address(this)),
-            amount,
-            "Wrong staked amount"
-        );
-
-        // Check other starting values
-        assertEq(
-            lara.delegatedAmounts(address(this)),
-            0,
-            "Wrong delegated amount"
-        );
-        assertEq(
-            lara.claimableRewards(address(this)),
-            0,
-            "Wrong claimable rewards"
-        );
-        assertEq(
-            lara.undelegated(address(this)),
-            0,
-            "Wrong undelegated amount"
-        );
-
-        address firstDelegator = lara.getDelegatorAtIndex(0);
-        assertEq(firstDelegator, address(this), "Wrong delegator address");
-
         // remove the stake instantly
         uint256 laraBalanceBeforeRemove = address(lara).balance;
         stTaraToken.approve(address(lara), amount);
@@ -105,19 +52,13 @@ contract LaraTest is Test, TestSetup {
             0,
             "Wrong stTara balance after claim"
         );
-    }
 
-    function invariant_testStakeAndRemoveStake() public {
-        uint256 laraBalanceBefore = address(lara).balance;
-        uint256 stTaraBalanceBefore = stTaraToken.balanceOf(address(this));
-        testFuzz_testStakeAndRemoveStake(1000 ether);
-        uint256 laraBalanceAfter = address(lara).balance;
         uint256 stTaraBalanceAfter = stTaraToken.balanceOf(address(this));
 
         // INVARIANT 1: Lara TARA balance should be the same
         assertEq(
             laraBalanceBefore,
-            laraBalanceAfter,
+            laraBalanceAfterRemove,
             "Lara balance should be the same"
         );
 
