f728ab8ff4abd10eb984131af316b55cc898445b
VargaElod23 <vargaelod23@gmail.com>
2024-02-13 11:59:51 +0100

    adds upgrades

--- scripts/foundry/PRNetDeploymentAware.sol
+++ scripts/foundry/PRNetDeploymentAware.sol
@@ -5,9 +5,9 @@ abstract contract DeploymentAware {
     address public immutable DPOS_ADDRESS =
         address(0x00000000000000000000000000000000000000fe);
     address public immutable stTARA_ADDRESS =
-        address(0x2aB6d071D1cb54E56F18ACA4D8482C3db37F6261);
+        address(0xD312eDC59c8AAB3FC9e44773EAD796a445aED09E);
     address public immutable ORACLE_ADDRESS =
-        address(0x054708c7F560C30dD6bf6000100369E2B9d49465);
+        address(0x3187C7486F6Aa40C520892766B9d8dcD3C23D9F1);
     address public immutable LARA_ADDRESS =
-        address(0xa7d833782a45f112c4890Bc7b1f0ce6c62Dd0Da3);
+        address(0x352AF15174C6415A3e33970636F2019337a60C45);
 }
